6ddd0aab60bc19f1c6a800d55b64f106d2bd5a97
Zeeshan Rasool <zrasool88@gmail.com>
2015-03-09 15:27:56 +0000

    fixed incorrect indentation showing up on github

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
   let(:station) {DockingStation.new(capacity: 50)}
 
   it "should allow setting default capacity on initializing" do
-      expect(station.capacity).to eq(50)
+    expect(station.capacity).to eq(50)
   end
 
 end
