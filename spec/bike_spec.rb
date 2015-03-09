f63062decfa890ae1aa93efe6dd4cc558feb18ab
Zeeshan Rasool <zrasool88@gmail.com>
2015-03-09 15:26:23 +0000

    still fixing indentation

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,21 +1,20 @@
 require 'bike'
 
 describe Bike do
-
   let(:bike) { Bike.new }
 
   it "should not be broken after we create it" do
     expect(bike).not_to be_broken
-	end
+  end
 
   it "should be able to break" do
-  	bike.break!
-  	expect(bike).to be_broken
+    bike.break!
+    expect(bike).to be_broken
   end
 
   it "should be able to get fixed" do
-  	bike.break!
-  	bike.fix!
-  	expect(bike).not_to be_broken
+    bike.break!
+    bike.fix!
+    expect(bike).not_to be_broken
   end
 end
